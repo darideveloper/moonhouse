5a382558a9587a170fe10d98392023eaaa83ac20
Silvia <silviaislam.eera@gmail.com>
2025-09-03 01:01:08 +0600

    refactor(ui): add clsx, class list

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,4 +1,5 @@
 import { FaBars, FaTimes } from "react-icons/fa";
+import clsx from "clsx";
 
 // Props for the Logo component
 interface LogoProps {
@@ -9,19 +10,48 @@ interface LogoProps {
 // Logo component
 export default function Logo({ handleNav, isNavOpen }: LogoProps) {
   return (
-    <div className="absolute w-full top-0 left-0 px-[3%] py-[1%]">
-      <div className="flex justify-between items-center relative">
+    <div className={clsx(
+      "absolute",
+      "w-full",
+      "top-0",
+      "left-0",
+      "px-[3%]",
+      "py-[1%]"
+    )}>
+      <div className={clsx(
+        "flex",
+        "justify-between",
+        "items-center",
+        "relative"
+      )}>
+        <a href="/">
         <img
           src="/images/logo.png"
           alt="Logo"
-          className="w-16 h-16 lg:w-20 lg:h-20 xl:w-32 xl:h-32"
-        />
+          className={clsx(
+            "w-16",
+            "h-16",
+            "lg:w-20",
+            "lg:h-20",
+            "xl:w-32",
+            "xl:h-32"
+          )}
+          />
+        </a>
         {/* Toggle the menu icon */}
         {/* Show the close icon if the menu is open */}
         {isNavOpen ? (
-          <FaTimes className="text-white text-xl lg:hidden" onClick={handleNav} />
+          <FaTimes className={clsx(
+            "text-white",
+            "text-xl",
+            "lg:hidden"
+          )} onClick={handleNav} />
         ) : (
-          <FaBars className="text-white text-xl lg:hidden" onClick={handleNav} />
+          <FaBars className={clsx(
+            "text-white",
+            "text-xl",
+            "lg:hidden"
+          )} onClick={handleNav} />
         )}
       </div>
     </div>
--- src/components/ui/NavButton.tsx
+++ src/components/ui/NavButton.tsx
@@ -1,3 +1,8 @@
+
+
+import clsx from "clsx";
+
+
 // Props for the NavButton component
 interface NavButtonProps {
   href: string;
@@ -8,7 +13,17 @@ interface NavButtonProps {
 export default function NavButton({ href, children }: NavButtonProps) {
   return (
     <li>
-      <a href={href} className="text-white text-sm md:text-base lg:text-lg p-2 hover:text-black hover:bg-white hover:p-2 hover:rounded">
+      <a href={href} className={clsx(
+        "text-white",
+        "text-sm",
+        "md:text-base",
+        "lg:text-lg",
+        "p-2",
+        "hover:text-black",
+        "hover:bg-white",
+        "hover:p-2",
+        "hover:rounded"
+      )}>
         {children}
       </a>
     </li>
