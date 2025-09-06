708bccd85547e0cd7e3b1295adef6145f065a094
daridev <darideveloper@gmail.com>
2025-09-06 12:10:01 -0600

    feat (settings/plugins): add sitemap and tailwind-breakpoints

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
+import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [
+    react(),
+    sitemap(),
+    showTailwindcssBreakpoint()
+  ]
 });
