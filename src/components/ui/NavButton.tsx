e522023fee7f50186bfea62bb5d31d3e7c0f49b4
daridev <darideveloper@gmail.com>
2025-09-10 15:26:00 -0600

    style (NavButton): update bg color and animation

--- src/components/ui/NavButton.tsx
+++ src/components/ui/NavButton.tsx
@@ -15,14 +15,18 @@ export default function NavButton({ href, children }: NavButtonProps) {
     <li>
       <a 
         href={href} 
+        // Update styles to make it looks more readable
         className={clsx(
           "text-white hover:text-black",
           "text-sm md:text-base lg:text-lg",
-          "p-2",
-          "hover:bg-white",
+          "py-2",
+          "px-6",
+          // DARIDEV: variants at the same line
+          "bg-brand-black/20 hover:bg-white",
           "hover:rounded",
           "hover:transition",
-          "hover:duration-300"
+          // DARIDEV: apply duration always (before hover)
+          "duration-300"
         )}
       >
         {children}
