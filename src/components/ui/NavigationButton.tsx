0d4340fca6abd707e657a58e9798b8532ea5d4c4
daridev <darideveloper@gmail.com>
2025-09-11 16:52:17 -0600

    style (NavigationButton): change mix-blend

--- src/components/ui/NavigationButton.tsx
+++ src/components/ui/NavigationButton.tsx
@@ -1,32 +1,40 @@
+// DARIDEV: code formatted
+
 // icons
-import { FaRegArrowAltCircleRight, FaRegArrowAltCircleLeft } from "react-icons/fa";
-import clsx from "clsx";
+import {
+  FaRegArrowAltCircleRight,
+  FaRegArrowAltCircleLeft,
+} from 'react-icons/fa'
+import clsx from 'clsx'
 
 interface NavigationButtonProps {
-    direction: 'prev' | 'next';
-    className?: string;
+  direction: 'prev' | 'next'
+  className?: string
 }
 
 export default function NavigationButton({
-    direction,
-    className,
+  direction,
+  className,
 }: NavigationButtonProps) {
-    const Icon = direction === 'next' ? FaRegArrowAltCircleRight : FaRegArrowAltCircleLeft;
+  const Icon =
+    direction === 'next' ? FaRegArrowAltCircleRight : FaRegArrowAltCircleLeft
 
-    return (
-        <button className={clsx(
-            className,
-            "absolute",
-            direction === 'next' ? 'right-4' : 'left-4',
-            "top-1/2",
-            "-translate-y-1/2",
-            "z-10",
-            "text-brand-grey",
-            "!opacity-100",
-            "mix-blend-overlay",
-            "cursor-pointer"
-        )}>
-            <Icon className="text-3xl" />
-        </button>
-    );
+  return (
+    <button
+      className={clsx(
+        className,
+        'absolute',
+        direction === 'next' ? 'right-4' : 'left-4',
+        'top-1/2',
+        '-translate-y-1/2',
+        'z-10',
+        'text-brand-grey',
+        '!opacity-100',
+        'mix-blend-luminosity', // DARIDEV: changed from overlay to luminosity for better visibility
+        'cursor-pointer'
+      )}
+    >
+      <Icon className='text-3xl' />
+    </button>
+  )
 }
