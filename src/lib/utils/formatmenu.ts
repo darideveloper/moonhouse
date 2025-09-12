161da60394b59803afb988f02ac0e2d383aa806e
daridev <darideveloper@gmail.com>
2025-09-12 14:34:08 -0600

    feat (Menu): get special offers from api

--- src/lib/utils/formatmenu.ts
+++ src/lib/utils/formatmenu.ts
@@ -1,48 +1,55 @@
-import type { MenuCategory, MenuItem } from "../../types/apitypes";
+import type { MenuCategory, MenuItem } from '../../types/apitypes'
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export interface FormattedMenuItem {
-  name: string;
-  description: string;
-  price: number;
+  name: string
+  description: string
+  price: number
   image: {
-    src: string;
-    alt: string;
-  };
+    src: string
+    alt: string
+  }
 }
 
 export interface FormattedMenuSection {
-  title: string;
-  items: FormattedMenuItem[];
+  title: string
+  items: FormattedMenuItem[]
+}
+
+
+// Format single menu utem
+export const structureMenuItemData = (apiItem: MenuItem): FormattedMenuItem => {
+  return {
+    name: apiItem.name,
+    description: apiItem.details,
+    price: parseFloat(apiItem.price),
+    image: {
+      src: `${API_BASE_URL}/assets/${apiItem.photo}`,
+      alt: apiItem.name,
+    },
+  }
 }
 
 export const structureMenuData = (
   categories: MenuCategory[],
   allItems: MenuItem[]
 ): FormattedMenuSection[] => {
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   const formattedMenu = categories.map((category) => {
     const itemsForCategory = allItems.filter(
       (item) => item.category === category.id
-    );
+    )
 
     const formattedItems = itemsForCategory.map((apiItem) => {
-      return {
-        name: apiItem.name,
-        description: apiItem.details,
-        price: parseFloat(apiItem.price),
-        image: {
-          src: `${API_BASE_URL}/assets/${apiItem.photo}`,
-          alt: apiItem.name,
-        },
-      };
-    });
+      return structureMenuItemData(apiItem)
+    })
 
     return {
       title: category.name,
       items: formattedItems,
-    };
-  });
+    }
+  })
 
-  return formattedMenu;
-};
+  return formattedMenu
+}
