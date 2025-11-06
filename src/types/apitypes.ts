c468f862181479d79730a190240ccf1f2aca72b5
SagDev <sagdev122@gmail.com>
2025-11-06 16:25:27 -0600

    feat(apitpes): new data

--- src/types/apitypes.ts
+++ src/types/apitypes.ts
@@ -1,43 +1,65 @@
 export type DayCode = "MO" | "TU" | "WE" | "TH" | "FR" | "SA" | "SU";
 
 export interface OpeningHour {
-    id: number;
-    status: "published" | "draft" | "archived";
-    day: DayCode;
-    start_hour: string;
-    end_hour: string;
+  id: number;
+  status: "published" | "draft" | "archived";
+  day: DayCode;
+  start_hour: string;
+  end_hour: string;
 }
 
 export interface OpeningHoursResponse {
-    data: OpeningHour[];
+  data: OpeningHour[];
 }
 
 export interface FormattedSchedule {
-    day: string;
-    hours: string;
+  day: string;
+  hours: string;
 }
 
 export interface MenuItem {
-    id: number;
-    status: 'published' | 'draft' | 'archived';
-    name: string;
-    details: string;
-    price: string;
-    photo: string | null;
-    is_today_special: boolean;
-    category: number | null;
+  id: number;
+  status: "published" | "draft" | "archived";
+  name: string;
+  details: string;
+  price: string;
+  photo: string | null;
+  is_today_special: boolean;
+  category: number | null;
 }
 
 export interface MenuCategory {
-    id: number;
-    status: 'published' | 'draft' | 'archived';
-    name: string;
+  id: number;
+  status: "published" | "draft" | "archived";
+  name: string;
 }
 
 export interface MenuItemsResponse {
-    data: MenuItem[];
+  data: MenuItem[];
 }
 
 export interface MenuCategoriesResponse {
-    data: MenuCategory[];
+  data: MenuCategory[];
+}
+
+export interface AboutText {
+  id: number;
+  key: string;
+  text: string;
+  status: string;
+}
+
+export interface AboutTextsResponse {
+  data: AboutText[];
+}
+
+export interface AboutImage {
+  id: number;
+  key: string;
+  image: string;
+  status: string;
+}
+
+export interface LandingImageResponse {
+  data: AboutImage[];
 }
